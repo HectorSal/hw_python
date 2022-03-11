c2b469c237cf7f78425ab5fb0dab3d4c17937ae4
Hector Saldivar <hector.saldivar@sjsu.edu>
2022-03-10 17:43:34 -0800

    removed duplicate values in document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -11,12 +11,21 @@ word_count_values = word_count.values()
 word_count_values = sorted(word_count_values, reverse = True) # sorted values
 
 n = len(word_count_values)
+no_duplicate_values = {}
+for i in range(n):
+	no_duplicate_values[word_count_values] = i
+word_count_values_no_duplicates = []
+i = 0
+for key in no_duplicate_values:
+	word_count_values_no_duplicates[i] = key
+	i = i +1
+
 word_count_sorted = {}
 #go through each value starting from the largest value and add the word that maps to that value to a new dictionary
 for i in range(n):
 	for key in word_count:
-		if word_count.get(key) == word_count_values[i]:
-			word_count_sorted[key] = word_count_values[i]
+		if word_count.get(key) == word_count_values_no_duplicates[i]:
+			word_count_sorted[key] = word_count_values_no_duplicates[i]
 print('\r')
 i = 0
 for key in word_count_sorted:
